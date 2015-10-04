8b24308165f0ab88fe3e69361d9f520ad1cb4918
jcquinlan <quinlanjc@vcu.edu>
2015-10-03 18:16:00 -0700

    added name selection functions

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-var names = 'james ally cole tyler quinn';
-names = names.split(' ');
+window.onload = function(){
 
-var roll = document.getElementById('roll');
-var name = document.getElementById('char-name');
+    var firstNames = 'james ally cole tyler quinn';
+    var lastNames = 'quinlan sheehan bush kofron kender';
 
-var gender = document.customize.gender;
-var race = document.customize.race;
+    firstNames = firstNames.split(' ');
+    lastNames = lastNames.split(' ');
 
-var checkGender = function(){
-    for(var i = 0; i < gender.options.length; i++){
-        if(gender.options[i].selected){
-            return gender.options[i].value;
-            console.log(gender.options[i].value);
-            break;
+    var fName, lName;
+
+    var gender = document.customize.gender;
+    var race = document.customize.race;
+
+    var checkGender = function(){
+        for(var i = 0; i < gender.options.length; i++){
+            if(gender.options[i].selected){
+                return gender.options[i].value;
+                break;
+            }
         }
     }
-}
 
-roll.addEventListener('click', checkGender);
+    var checkRace = function(){
+        for(var i = 0; race.options.length; i++){
+            if(race.options[i].selected){
+                return race.options[i].value;
+                alert('BLAH');
+                break;
+            }
+        }
+    }
+
+    var genName = function(){
+        var num1 = Math.floor(Math.random() * firstNames.length);
+        var num2 = Math.floor(Math.random() * lastNames.length);
+
+        var fName = firstNames[num1];
+        var lName = lastNames[num2];
+
+        return fName + " " + lName;
+
+    }
+
+    //onload test function
+    alert('yo!');
+}
